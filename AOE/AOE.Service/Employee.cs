f107197d6689521947ae84329c3ad2167be30410
OlegIvanov <iosmsft@outlook.com>
2014-01-15 15:20:44 +0200

    service

---
+++ AOE/AOE.Service/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOE.Service
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public double Salary { get; set; }
+    }
+}
