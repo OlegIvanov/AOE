e30c323f99fe1efcfb86dd953f7320be8dc7bd09
OlegIvanov <iosmsft@outlook.com>
2014-01-15 15:14:12 +0200

    all

---
+++ AOE/AOE.Service/EmployeeListRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOE.Service
+{
+    public class EmployeeListRequest
+    {
+        public int JobId { get; set; }
+        public SortColumn SortColumn { get; set; }
+        public SortOrder SortOrder { get; set; }
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+    }
+}
---
+++ AOE/AOE.Service/SortColumn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOE.Service
+{
+    public enum SortColumn
+    {
+        FullName = 0,
+        Salary = 1
+    }
+}
