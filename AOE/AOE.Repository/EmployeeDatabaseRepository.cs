0a0601f979385115c3708e103aeb739e7274fb03
OlegIvanov <iosmsft@outlook.com>
2014-01-15 15:34:18 +0200

    service

---
+++ AOE/AOE.Repository/EmployeeDatabaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using AOE.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOE.Repository
+{
+    public class EmployeeDatabaseRepository : IEmployeeRepository
+    {
+        public EmployeeListResponse GetEmployeeList(EmployeeListRequest employeeListRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
