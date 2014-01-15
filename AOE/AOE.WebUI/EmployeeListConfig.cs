0aff9a4ad9b0af8a8f1a5f7a104cd23bf68d094e
OlegIvanov <iosmsft@outlook.com>
2014-01-15 14:16:08 +0200

    parsing

---
+++ AOE/AOE.WebUI/EmployeeListConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace AOE.WebUI
+{
+    [XmlRoot("Config")]
+    public class EmployeeListConfig
+    {
+        [XmlAttribute("type")]
+        public EmployeeListDataSource DataSource { get; set; }
+
+        [XmlAttribute("url")]
+        public string Url { get; set; }
+
+        [XmlElement("PageSize")]
+        public int PageSize { get; set; }
+
+        [XmlElement("IsEditable")]
+        public bool IsEditable { get; set; }
+
+        public static EmployeeListConfig GetConfig(string xmlConfigFile) 
+        {
+            string absolutePath = HttpContext.Current.Server.MapPath(xmlConfigFile);
+            return DeserializeFromXml(absolutePath);
+        }
+
+        private static EmployeeListConfig DeserializeFromXml(string xmlConfigFile)
+        {
+            using (TextReader textReader = new StreamReader(xmlConfigFile))
+            {
+                XmlSerializer deserializer = new XmlSerializer(typeof(EmployeeListConfig));
+                return (EmployeeListConfig)deserializer.Deserialize(textReader);
+            }
+        }
+    }
+}
---
+++ AOE/AOE.WebUI/EmployeeListDataSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AOE.WebUI
+{
+    public enum EmployeeListDataSource
+    {
+        Database = 0,
+        WebService = 1
+    }
+}
