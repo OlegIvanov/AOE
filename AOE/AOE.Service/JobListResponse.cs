88458f4bfda31c47e97bfff50b1dc531c861d248
OlegIvanov <iosmsft@outlook.com>
2014-01-15 22:42:43 +0200

    sql repo

---
+++ AOE/AOE.Service/JobListResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOE.Service
+{
+    public class JobListResponse
+    {
+        public IList<Job> Jobs { get; set; }
+    }
+}
