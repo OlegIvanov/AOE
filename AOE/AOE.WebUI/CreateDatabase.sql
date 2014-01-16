bd3b7ee4cb0a006be65f60c0f623f82cbccd6166
OlegIvanov <iosmsft@outlook.com>
2014-01-16 14:35:24 +0200

    data

--- AOE/AOE.WebUI/CreateDatabase.sql
+++ AOE/AOE.WebUI/CreateDatabase.sql
@@ -45,13 +45,24 @@ INSERT INTO Jobs VALUES (1, 'PHP Developer')
 INSERT INTO Jobs VALUES (2, '.NET Software Engineer')
 INSERT INTO Jobs VALUES (3, 'Chief Executive Officer')
 
-INSERT INTO Employees Values (1, 1, 'Billy', 'Bob', 25.09)
+INSERT INTO Employees Values (1, 1, 'Billy', 'Bob', 1125.09)
 INSERT INTO Employees Values (2, 1, 'Billy', 'Ralph', 39.23)
 INSERT INTO Employees Values (3, 1, 'Axel', 'Norway', 100.67)
 INSERT INTO Employees Values (4, 1, 'Gary', 'Statham', 11.20)
 INSERT INTO Employees Values (5, 1, 'Carla', 'Davolio', 200.09)
 INSERT INTO Employees Values (6, 1, 'Drew', 'Barnsley', 300.01)
 INSERT INTO Employees Values (7, 1, 'Frank', 'Sinatra', 1)
+
+INSERT INTO Employees Values (8, 2, 'Nancy', 'Drew', 900)
+INSERT INTO Employees Values (9, 2, 'Edu', 'Nilson', 235)
+INSERT INTO Employees Values (10, 2, 'Dimitar', 'Berbatov', 567)
+INSERT INTO Employees Values (11, 2, 'Gary', 'Oldman', 987)
+
+INSERT INTO Employees Values (12, 3, 'Abraham', 'Lincoln', 788)
+INSERT INTO Employees Values (13, 3, 'Leo', 'Messi', 221.45)
+INSERT INTO Employees Values (14, 3, 'Dirk', 'Novicky', 567.98)
+INSERT INTO Employees Values (15, 3, 'Alexander', 'Gleb', 9.90)
+INSERT INTO Employees Values (16, 3, 'Pavlik', 'Morozov', 200.20)
 
 GO
 CREATE PROCEDURE GetEmployeeList
