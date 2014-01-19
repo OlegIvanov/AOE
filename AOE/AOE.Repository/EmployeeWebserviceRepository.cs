32d4c633701fe86036b3bfda6da8765d1595269e
OlegIvanov <iosmsft@outlook.com>
2014-01-19 14:15:36 +0200

    proxy

--- AOE/AOE.Repository/EmployeeWebserviceRepository.cs
+++ AOE/AOE.Repository/EmployeeWebserviceRepository.cs
@@ -3,34 +3,59 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using EWS = EmployeeWebServiceProxyTypesNamespace;
 
 namespace AOE.Repository
 {
     public class EmployeeWebserviceRepository : IEmployeeRepository
     {
+        private EWS.EmployeeWebService _employeeWebServiceProxy;
+
+        public EmployeeWebserviceRepository(string url)
+        {
+            _employeeWebServiceProxy = new EWS.EmployeeWebService();
+        }
+
         public List<Job> FindAllJobs()
         {
-            throw new NotImplementedException();
+            return _employeeWebServiceProxy.GetAllJobs()
+                .ConvertJobProxyArrayToJobList();
         }
 
         public List<Employee> FindBy(EmployeeQuery employeeQuery)
         {
-            throw new NotImplementedException();
+            EWS.EmployeeQuery employeeQueryProxy = new EWS.EmployeeQuery 
+            { 
+                JobId = employeeQuery.JobId,
+                SortExpression = employeeQuery.SortExpression,
+                PageIndex = employeeQuery.PageIndex,
+                PageSize = employeeQuery.PageSize
+            };
+
+            return _employeeWebServiceProxy.GetEmployeeList(employeeQueryProxy)
+                .ConvertEmployeeProxyArrayToEmployeeList();
         }
 
         public int GetCountByJobId(int jobId)
         {
-            throw new NotImplementedException();
+            return _employeeWebServiceProxy.GetEmployeeCountByJobId(jobId);
         }
 
         public Employee FindBy(int employeeId)
         {
-            throw new NotImplementedException();
+            return _employeeWebServiceProxy.GetEmployeeBy(employeeId)
+                .ConvertEmployeeProxyToEmployee();
         }
 
         public void Update(Employee employee)
         {
-            throw new NotImplementedException();
+            _employeeWebServiceProxy.UpdateEmployee(new EWS.Employee 
+            { 
+                Id = employee.Id,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                Salary = employee.Salary
+            });
         }
     }
 }
---
+++ AOE/AOE.Repository/ProxyTypesMapperExtensionMethods.cs
@@ -0,0 +1,56 @@
+﻿using AOE.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EWS = EmployeeWebServiceProxyTypesNamespace;
+
+namespace AOE.Repository
+{
+    public static class ProxyTypesMapperExtensionMethods
+    {
+        public static List<Job> ConvertJobProxyArrayToJobList(this EWS.Job[] jobProxyArray)
+        {
+            List<Job> jobs = new List<Job>();
+
+            foreach (EWS.Job jobProxy in jobProxyArray)
+            {
+                jobs.Add(jobProxy.ConvertJobProxyToJob());
+            }
+
+            return jobs;
+        }
+
+        public static Job ConvertJobProxyToJob(this EWS.Job jobProxy)
+        {
+            return new Job
+            {
+                Id = jobProxy.Id,
+                Name = jobProxy.Name
+            };
+        }
+
+        public static List<Employee> ConvertEmployeeProxyArrayToEmployeeList(this EWS.Employee[] employeeProxyArray)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            foreach (EWS.Employee employeeProxy in employeeProxyArray)
+            {
+                employees.Add(employeeProxy.ConvertEmployeeProxyToEmployee());
+            }
+
+            return employees;
+        }
+
+        public static Employee ConvertEmployeeProxyToEmployee(this EWS.Employee employeeProxy)
+        {
+            return new Employee
+            {
+                Id = employeeProxy.Id,
+                FirstName = employeeProxy.FirstName,
+                LastName = employeeProxy.LastName,
+                Salary = employeeProxy.Salary
+            };
+        }
+    }
+}
